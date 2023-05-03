dc8bbad9af5133a8bd53f506273dee82e5f5273a
Artur Pilarek <artur.pilarek@gmail.com>
2023-05-03 09:29:09 +0200

    Added and tested Vuetify

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <v-switch label="Switch"></v-switch>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
