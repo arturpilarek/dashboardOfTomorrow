7139a220112f5055d58c41c8ef90bfb2dbac61e1
caldecreativo <93321331+caldecreativo@users.noreply.github.com>
2023-05-10 06:10:00 +0200

    wireframe and components

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,65 +1,30 @@
-<script setup>
-import { onMounted } from "vue";
+<script>
+// import { onMounted, provide, ref } from "vue";
 import { useCounterStore } from "./stores/counter";
 import { useExpressRoute } from "./stores/apiData";
 import axios from "axios";
+import SideBarNav from "./Components/Layout/SideBarNav.vue";
 
-const counterStore = useCounterStore();
-
-function incrementCounter() {
-  counterStore.increment();
-}
-
-const expressRoute = useExpressRoute();
-
-async function getData() {
-  await expressRoute.getData();
+export default {
+  components: {
+    SideBarNav,
+    TaskCard
+  }
 }
 
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    <v-switch label="Switch"></v-switch>
-    <div class="sass-test">
-      <p>Sass testing</p>
-    </div>
-    <div>
-      <p>Pinia testing</p>
-      <p>pinia counter: {{ counterStore.counter }}</p>
-      <button @click="incrementCounter">Increment counter</button>
-    </div>
-    <div>
-      <p>Express route testing</p>
-      <button @click="getData">get data</button>
-      <p>apiData: {{ expressRoute.apiData }}</p>
-    </div>
-  </div>
+
+  <side-bar-nav>
+
+  </side-bar-nav>
+
+
 </template>
 
-<style scoped lang="scss">
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 
-.sass-test {
-  p {
-    color: red;
-  }
-}
+<style lang="scss">
+
+
 </style>
---
+++ Client/src/Components/Elements/CreateTaskModal.vue
@@ -0,0 +1,79 @@
+<template>
+    <v-card
+    id="createCard"
+    
+    >
+        <v-card-title> {{ title }} </v-card-title>
+
+        <v-row 
+        no-gutters
+        class="d-flex justify-space-between"
+        >
+        <v-card-subtitle
+        v-for="sub in subtitles"
+        :key="sub.text"
+        >
+            <c-card-text>{{ sub.text }}</c-card-text>
+
+        </v-card-subtitle>
+        </v-row>
+
+    <v-textarea
+    class="pa-6"
+    id="textarea"
+    label="Description"
+    >
+
+    </v-textarea>
+
+    <template
+    class="d-flex justify-end pr-6"
+    >
+    <v-btn
+    id="addBtn"
+    >
+    ADD TASK
+    </v-btn>
+    </template>
+
+    </v-card>
+
+
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Taskname',
+            subtitles: [
+                {
+                    text: 'STATUS'
+                },
+                {
+                    text: 'ASSIGNED TO'
+                },
+                {
+                    text: 'DATES'
+                }
+            ]
+    }
+}
+}
+</script>
+
+<style>
+#createCard {
+    margin-top: 30px;
+    width: 700px;
+    height: 450px;
+    background: #F6F6F6;
+    border-radius: 10px;
+}
+
+#textarea {
+    line-height: 45px;
+}
+
+</style>
