c54dec21b01e65a16933e429beafffeadd48e867
Dalena <79627422+dalenatrinh@users.noreply.github.com>
2023-05-11 12:38:46 +0200

    Refactor

--- Server/index.js
+++ Server/index.js
@@ -22,8 +22,8 @@ app.use(router)
 // Uncomment this to connect to MongoDB
 
 
- // Server without MongoDB
- app.listen(8082, () => {
-     console.log('server connected, port8082')
- }
- )
+// Uncomment this to connect to MongoDB
+mongoose.connect(process.env.MONGO_ENDPOINT).then(() => {
+    console.log('server connected, port8081')
+    app.listen(8081)
+})
