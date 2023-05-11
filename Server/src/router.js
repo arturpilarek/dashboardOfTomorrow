b72ed11135f8b67e08f539331a10cae8ec41e03a
Dalena <79627422+dalenatrinh@users.noreply.github.com>
2023-05-11 12:37:19 +0200

    Resolved

--- Server/src/router.js
+++ Server/src/router.js
@@ -12,9 +12,22 @@ const userIsAuth = require('./routes/user/isAuth') //to authenticate, that user 
 
 //Todos
 const createTodoRoute = require('./routes/todos/createTodosRoute')
-// const readTodosRoute = require('./routes/todos/readTodosRoute')
+const readTodosRoute = require('./routes/todos/readTodosRoute')
 const deleteTodoRoute = require('./routes/todos/deleteTodoRoute')
 const updateTodoRoute = require('./routes/todos/updateTodosRoute')
+const readTodoByIDRoute = require('./routes/todos/readTodoByIDRoute')
+
+//Taskboards
+const createTodosBoardRoute = require("./routes/taskboards/createTaskboardRoute");
+const readAllTodosOnBoardRoute = require("./routes/taskboards/readAllTasksOnBoardRoute");
+const updateTaskboardRoute = require("./routes/taskboards/updateTaskboardRoute");
+const deleteTaskboardRoute = require("./routes/taskboards/deleteTaskboardRoute");
+
+//Teams Routes
+const createTeamsRoute = require("./routes/teams/createTeamsRoute")
+// const readAllTeamsTodosRoute = require("./routes/teams/readTeamTodosRoute")
+// const updateTeamsRoute = require("./routes/teams/updateTeamRoute")
+// const deleteTeamsRoute = require("./routes/teams/deleteTeamRoute")
 
 //Taskboards
 const createTodosBoardRoute = require("./routes/taskboards/createTaskboardRoute");
@@ -46,6 +59,8 @@ router.post('/login', userLoginRoute)
 // router.post('/todos/get', tokenAuth, readTodosRoute)
 router.post('/todos/create', createTodoRoute)
 // router.post('/todos/create',tokenAuth, createTodoRoute)
+router.get('/todos', readTodosRoute)
+router.get('/todos/:id', readTodoByIDRoute)
 router.put('/todos/:id', updateTodoRoute)
 router.delete('/todos/:id', deleteTodoRoute)
 
