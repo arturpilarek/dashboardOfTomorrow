5a81dda56606ae873f49bcc42c9aaba9caaab02c
Dalena <79627422+dalenatrinh@users.noreply.github.com>
2023-05-11 12:38:46 +0200

    Refactor

--- Server/src/routes/todos/readTodosRoute.js
+++ Server/src/routes/todos/readTodosRoute.js
@@ -3,10 +3,7 @@ const TodoModel = require('../../models/TodoModel')
 module.exports = async (req, res) => {
     try {
         // Logik for at finde alle todos
-<<<<<<< HEAD
-=======
         let todos = await TodoModel.find();
->>>>>>> 9777bd3 (todo read single+/All added)
 
         res.json(todos)
     }
