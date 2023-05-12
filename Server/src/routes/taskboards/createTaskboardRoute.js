8c9157dad9d2ef036a965269b8f93e9c972bda28
MrKraack <MrKraack@gmail.com>
2023-05-12 13:20:40 +0200

    Delete taskboard route done

--- Server/src/routes/taskboards/createTaskboardRoute.js
+++ Server/src/routes/taskboards/createTaskboardRoute.js
@@ -8,16 +8,29 @@ module.exports = async (req,res) => {
     //Get User ID from req - Not yet implemented
     // let userCreatedTaskboardID = req.query.UserID
 
-    //Get taskboard values from req
-    // let taskboardId = req.params.id;
-    console.log(req.params);
+    //Search database for objects matching TodoModel
+    const taskboardCounter = await TaskboardModel.find({TaskboardModel});
+
+    //Get the last element from array
+    let lastTaskboardObject = taskboardCounter.slice(-1);
+
+    //Regex to get number from end of String
+    let numberRegex = /\d+$/;
+
+    //Use regex and convert string to Int
+    let taskboardIdFromDB = parseInt(lastTaskboardObject[0].taskboardID.match(numberRegex));
+
+    //Increment the ID
+    taskboardIdFromDB++;
+    let taskboardID = `taskboard${taskboardIdFromDB}`;
+
 
 
 
     try {       
         //Create a taskboard
         let newTaskboard = new TaskboardModel({
-            taskboardID: "taskboardKapper1",
+            taskboardID: taskboardID,
             taskboardName: taskboardName,
             taskboardTasks: taskboardTasks
         });
---
+++ Server/src/routes/taskboards/deleteTaskboardRoute.js
@@ -0,0 +1,18 @@
+const TaskboardModel = require("../../models/TaskboardModel");
+
+module.exports = async(req,res) => {
+    //get id from req.params
+    let taskboardId = req.params.taskboardId
+
+    
+
+    try {
+        //Find and delete taskboard with matching ID
+        await TaskboardModel.findOneAndDelete({taskboardID:taskboardId});
+        res.json("Deleted Taskboard: " * taskboardId)
+
+    } catch (err) {
+        res.status(500).json({error: err.message})
+        
+    }
+}
