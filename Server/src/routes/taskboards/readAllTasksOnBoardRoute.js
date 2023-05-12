6192f9210b2908c3c6bca196823241a6cef0bc77
MrKraack <MrKraack@gmail.com>
2023-05-12 11:16:48 +0200

    Read all todos on taskboard done

--- Server/src/routes/taskboards/readAllTasksOnBoardRoute.js
+++ Server/src/routes/taskboards/readAllTasksOnBoardRoute.js
@@ -1,6 +1,31 @@
 const TodoModel = require("../../models/TodoModel")
-const TaskboardModel = require("../../models/TaskboardModel")
+const TaskboardModel = require("../../models/TaskboardModel");
 
 module.exports = async (req, res) => {
-    let
+    console.log(req.params);
+    console.log(req.query);
+
+    let taskboardID = req.params.boardID
+
+    try {
+        //Get taskboard matching ID
+        let taskboardObject = await TaskboardModel.findOne({taskboardID: taskboardID});
+        console.log(taskboardObject);
+
+        //Loop through task array and get them all
+        for (let i = 0; i < taskboardObject.taskboardTasksID.length; i++) {
+            console.log(`This is task ${i}: `)
+            console.log(taskboardObject.taskboardTasksID[i]);
+            //fetch todo matching id
+            let currentTodo = await TodoModel.findOne({todoID: taskboardObject.taskboardTasksID[i]})
+            console.log(currentTodo)
+            
+        }
+
+
+        res.json(taskboardObject)
+    } catch (err) {
+        res.status(500).json({error: err.message})
+        
+    }
 }
