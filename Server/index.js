0862eb8010a2289603ea38daf8f0a33b60898ce9
MrKraack <MrKraack@gmail.com>
2023-05-17 10:54:38 +0200

    Resolved conflicts

--- Server/index.js
+++ Server/index.js
@@ -24,10 +24,3 @@ mongoose.connect(process.env.MONGO_ENDPOINT).then(() => {
      console.log('server connected, port8081')
      app.listen(8081)
 })
-
-
-// // Server without MongoDB
-// app.listen(8082, () => {
-//     console.log('server connected, port8082')
-// }
-// )
