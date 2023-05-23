8d0f1cd12d8512cd7fa57cfb6750262c715dea1d
Artur Pilarek <artur.pilarek@gmail.com>
2023-05-23 21:54:47 +0200

    Added working example with pinia data avaiable after login

--- Client/src/views/Startingpage.vue
+++ Client/src/views/Startingpage.vue
@@ -1,10 +1,17 @@
 <template>
-    <div>
-      <h2>Starting page</h2>
-    </div>
-  </template>
-<script>
-export default {
-  name: "StartingPage"
-}
+  <div>
+    <h2>Starting page</h2>
+    <p v-for="(taskboard, index) in boards" :key="index">
+      {{ taskboard.taskboardID }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { useTaskDataStore } from "../stores/tasksData.store";
+
+const { boards } = useTaskDataStore();
+
+console.log(boards);
 </script>
+  
