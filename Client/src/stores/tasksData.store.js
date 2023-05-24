7cfdb7b421521cbf6c1fb44f61ea9385d49ba7a7
Anna-1902 <annaipsen19@gmail.com>
2023-05-24 13:04:35 +0200

    update actions

--- Client/src/stores/tasksData.store.js
+++ Client/src/stores/tasksData.store.js
@@ -1,79 +1,121 @@
 import {defineStore} from 'pinia'
 import {request} from "../_helpers/fetchWrapper.js";
+import {useAuthStore} from "./auth.store.js";
+
+const {user} = useAuthStore()
 
 
 export const useTaskDataStore = defineStore({
     id: 'teams',
     state: () => ({
-        boards: [], teams: []
+        boards: [],
+        teams: []
     }),
     actions: {
-
-        async getAll (id) {
+        //get all actions
+        async getAll () {
             const data = await request.get(`/fetchAll/user1`)
 
-            this.boards.push(...data.taskboards)
-            this.teams.push(...data.teams)
+            this.boards = data.taskboards
+            this.teams = data.teams
 
             console.log(this.boards)
             console.log(this.teams)
         },
+        async getAllTeams (){
+            this.teams = await request.get(`/fetchAllTeams/user1`)
+        },
+        async getAllPrivateBoards () {
+            this.boards = await request.get(`/fetchAllPrivateBoards/user1`)
+        },
 
         //teams actions
         async createTeam (name, description) {
-            this.teams = await request.post('/teams/create', {
+            await request.post('/teams/create', {
                 teamName: name,
                 teamDescription: description,
+                userID: user.userID
             })
+            await this.getAllTeams()
         },
-        async updateTeam (name, tasks, member, id) {
-            this.teams = await request.put(`/teams/${id}`, {
+        async updateTeam (name, description, members, teamId) {
+            const updatedTeam = await request.put(`/teams/${teamId}`, {
                 teamName: name,
-                teamDescription: tasks,
-                teamMember: member
+                teamDescription: description,
+                teamMember: members
             })
+            await this.getAllTeams()
         },
-        async deleteTeam (id) {
-            await request.delete(`/teams/${id}`)
+        async addUserToTeam (userId, teamId){
+            await request.post(`/teams/addToTeam/${teamId}`, {
+                userID: userId
+            })
+            await this.getAllTeams()
+        },
+        async removeUserFromTeam (userId, teamId){
+            await request.post(`/teams/removeUserFromTeam/${teamId}`, {
+                userID: userId
+            })
+            await this.getAllTeams()
+        },
+        async deleteTeam (teamId) {
+            await request.delete(`/teams/${teamId}`)
+            await this.getAllTeams()
         },
 
         // boards actions
-        async createBoard (name, tasks) {
-            this.privateBoards = await request.post('/taskboards/create', {
+        async createBoard (name, teamID) {
+            const newBoard = await request.post('/taskboards/create', {
                 taskboardName: name,
-                taskboardTasks: tasks,
+                teamID: teamID,
+                userID: user.userID
             })
+
+            if (teamID) {
+                return this.getAllTeams()
+            }
+            await this.getAllPrivateBoards()
         },
-        async updateBoard (name, tasks, id) {
-            this.privateBoards = await request.put(`/taskboards/${id}`, {
+        async updateBoard (name, boardID) {
+            await request.put(`/taskboards/${boardID}`, {
                 taskboardName: name,
-                taskboardTasks: tasks,
             })
+            await this.getAll()
         },
-        async deleteBoard (id) {
-            await request.delete(`/taskboards/${id}`)
+        async deleteBoard (boardID) {
+            await request.delete(`/taskboards/${boardID}`)
+            await this.getAll()
         },
 
 
         //task actions
-        async createTask (name, description, state, completed) {
+        async createTask (name, description, state, completed, taskboardId, teamId) {
             this.tasks = await request.post('/todos/create', {
                 todoName: name,
                 todoDescription: description,
                 todoState: state,
                 todoCompleted: completed,
+                taskboardID: taskboardId,
+                userID: user.userID,
+                teamID: teamId
             })
+            if (teamID) {
+                return this.getAllTeams()
+            }
+            await this.getAllPrivateBoards()
         },
-        async updateTask (name, description, state, completed, id) {
-            this.tasks = await request.put(`/todos/${id}`, { //tjek lige her
+        async updateTask (name, description, state, completed, taskId) {
+            this.tasks = await request.put(`/todos/${taskId}`, {
                 todoName: name,
                 todoDescription: description,
                 todoState: state,
                 todoCompleted: completed,
             })
+            await this.getAll()
         },
-        async deleteTask (id) {
-            await request.delete(`/todos/${id}`)
+        async deleteTask (taskId) {
+            await request.delete(`/todos/${taskId}`)
+            await this.getAll()
         }
     }
 })
