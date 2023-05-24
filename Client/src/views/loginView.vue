260f2e695f21bee639c38f4fd774e21106b3dad8
Anna-1902 <annaipsen19@gmail.com>
2023-05-24 13:27:27 +0200

    resolve conflict

--- Client/src/views/loginView.vue
+++ Client/src/views/loginView.vue
@@ -6,7 +6,7 @@
       <v-sheet width="400" class="mx-auto d-flex flex-column justify-center">
         <h2 class="text-h2 mb-16 text-uppercase align-self-center text-purple">Welcome!</h2> <!-- change color of text -->
         <p class="text-overline">Please log in</p>
-        <v-form @submit.prevent="login">
+        <v-form fast-fail @submit.prevent="login">
           <v-text-field
               v-model="email"
               label="E-mail"
@@ -33,7 +33,7 @@ import DotAnimation from "../Components/animation/dotAnimation.vue";
 import { useAuthStore } from '../stores/auth.store.js'
 
 export default {
-  name: "Login",
+  name: "loginView",
   components: {DotAnimation},
   setup () {
     const authStore = useAuthStore();
@@ -50,7 +50,7 @@ export default {
     login(){
       return this.authStore.login(this.email, this.password)
     }
-  },
+  }
 
 }
 </script>
