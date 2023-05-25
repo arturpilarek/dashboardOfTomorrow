d80bf5cc9560d263fa9dea1d07ad604a1e9fcd3c
Artur Pilarek <artur.pilarek@gmail.com>
2023-05-25 19:59:56 +0200

    Minimal refactor in Sidebar

--- Client/src/Components/Layout/SideBarNav.vue
+++ Client/src/Components/Layout/SideBarNav.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-app id="inspire">
+  <v-app id="inspire">
     <v-navigation-drawer
       v-model="drawer"
       app
@@ -11,57 +11,57 @@
     >
       <v-list-item>
         <v-list-item-content>
-          <router-link to="/"><img :src="dotLogo"/></router-link>
+          <router-link to="/"><img :src="dotLogo" /></router-link>
         </v-list-item-content>
       </v-list-item>
 
-    <!-- Private -->
-    <v-list v-if="taskDataStore.boards[0]" v-model:opened="open">
+      <!-- Private -->
+      <v-list v-if="taskDataStore.boards[0]" v-model="open">
         <v-list-group value="private">
           <template v-slot:activator="{ props }">
             <v-list-item
-                v-bind="props"
-                prepend-icon="mdi-account"
-                title="Private"
+              v-bind="props"
+              prepend-icon="mdi-account"
+              title="Private"
             ></v-list-item>
           </template>
           <v-list-item
-              prepend-icon="mdi-circle"
-              v-for="project in taskDataStore.boards"
-              :key="project.taskboardID"
-              :title="project.taskboardName"
-              :value="project.taskboardName"
+            prepend-icon="mdi-circle"
+            v-for="project in taskDataStore.boards"
+            :key="project.taskboardID"
+            :title="project.taskboardName"
+            :value="project.taskboardName"
           ></v-list-item>
         </v-list-group>
-          
-    <!-- Teams -->
-            <v-list-group value="teams">
+
+        <!-- Teams -->
+        <v-list-group value="teams">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              prepend-icon="mdi-account-group"
+              title="Teams"
+            ></v-list-item>
+          </template>
+          <v-list-group
+            v-for="team in taskDataStore.teams"
+            :value="team.teamName"
+            :key="team.teamID"
+          >
             <template v-slot:activator="{ props }">
-              <v-list-item
-                  v-bind="props"
-                  prepend-icon="mdi-account-group"
-                  title="Teams"
-              ></v-list-item>
+              <v-list-item v-bind="props" :title="team.teamName"></v-list-item>
             </template>
-            <v-list-group v-for="team in taskDataStore.teams" :value="team.teamName" :key="team.teamID">
-                <template v-slot:activator="{ props }">
-                  <v-list-item
-                  v-bind="props"
-                  :title="team.teamName"
-                  ></v-list-item>
-                </template>
-                <v-list-item
-                  v-for="taskboard in team.teamTaskboards"
-                  prepend-icon="mdi-circle"
-                  :title="taskboard.taskboardName"
-                  :value="taskboard.taskboardName"
-                  :key="taskboard.taskboardID"
-              ></v-list-item>
-                
-            </v-list-group>
+            <v-list-item
+              v-for="taskboard in team.teamTaskboards"
+              prepend-icon="mdi-circle"
+              :title="taskboard.taskboardName"
+              :value="taskboard.taskboardName"
+              :key="taskboard.taskboardID"
+            ></v-list-item>
           </v-list-group>
-        </v-list>
-        
+        </v-list-group>
+      </v-list>
+
       <v-spacer></v-spacer>
 
       <v-container class="avatarContainer pa-7" fluid>
@@ -100,9 +100,7 @@
       </v-container>
     </v-navigation-drawer>
 
-    <v-main>
-
-    </v-main>
+    <v-main> </v-main>
   </v-app>
 </template>
 
@@ -127,54 +125,54 @@ const user = ref({
   fullName: "Anna Ipsen",
   email: "anna@anna.dk",
 });
-    onMounted(() => {
-      setTimeout(()=>{setRandomColors()}, 1500)
-    })
+onMounted(() => {
+  setTimeout(() => {
+    setRandomColors();
+  }, 1500);
+});
 
-    function setRandomColors() {
-      const mdiCircles = document.getElementsByClassName("mdi-circle");
-      for (let i = 0; i < mdiCircles.length; i++) {
-        let color = localStorage.getItem(`color-${i}`);
-        if (!color) {
-          color = randomColor();
-          localStorage.setItem(`color-${i}`, color);
-        }
-        mdiCircles[i].style.color = color;
-      }
+function setRandomColors() {
+  const mdiCircles = document.getElementsByClassName("mdi-circle");
+  for (let i = 0; i < mdiCircles.length; i++) {
+    let color = localStorage.getItem(`color-${i}`);
+    if (!color) {
+      color = randomColor();
+      localStorage.setItem(`color-${i}`, color);
     }
+    mdiCircles[i].style.color = color;
+  }
+}
 
-    function randomColor() {
-      const hue = Math.floor(Math.random() * 360);
-      const saturation = Math.floor(Math.random() * 100);
-      const lightness = Math.floor(Math.random() * 100);
-      return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-    }
+function randomColor() {
+  const hue = Math.floor(Math.random() * 360);
+  const saturation = Math.floor(Math.random() * 100);
+  const lightness = Math.floor(Math.random() * 100);
+  return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+}
 </script>
 
 <style lang="scss">
-  #navBar {
-    background: linear-gradient(180deg, #47126b 0%, #ea698b 100%);
-    position: relative;
-  }
+#navBar {
+  background: linear-gradient(180deg, #47126b 0%, #ea698b 100%);
+  position: relative;
+}
 
-  .v-list-item--density-default:not(.v-list-item--nav).v-list-item--one-line {
-    color: white;
-  }
+.v-list-item--density-default:not(.v-list-item--nav).v-list-item--one-line {
+  color: white;
+}
 
+.avatarContainer {
+  position: absolute;
+  bottom: 0;
+}
 
-  .avatarContainer {
-    position: absolute;
-    bottom: 0;
-  }
+.logo {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
 
-  .logo {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-
-  }
-
-  .v-list-items #team-item {
-    margin-left: -50px;
-  }
+.v-list-items #team-item {
+  margin-left: -50px;
+}
 </style>
